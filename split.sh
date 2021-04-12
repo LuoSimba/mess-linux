9e32cf7ef2da11415b511f511e3da6c9303cd745
simba-luo <r.puppy@qq.com>
2021-04-13 01:28:51 +0800

    Linux command split

--- split.sh
+++ split.sh
@@ -1,6 +1,24 @@
 # 
 # split 可用于分割很大的文本文件
 #
+# split a file into pieces
+
+# -a, --suffix-length=N
+#  use suffixes of length N (default 2)
+#
+# -b, --bytes=SIZE
+#  按照每份 SIZE 字节进行分割
+#
+# -l, --lines=NUMBER
+#  按照每份 NUMBER 行进行分割
+#
+# --verbose
+#  print a diagnostic just before each
+#  output file is opened
 
 split --verbose -a 3 -l 3000 server.log
+
+
+# 每份1000字节
+split -b 1000 DATA.log
 
