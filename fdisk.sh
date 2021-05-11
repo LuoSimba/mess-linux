3b93127a37016dc9c53c05ca8895c0a9b580ebfa
simba-luo <r.puppy@qq.com>
2021-05-12 03:03:20 +0800

    linux command fdisk

---
+++ fdisk.sh
@@ -0,0 +1,18 @@
+
+
+# fdisk - Partition table manipulator for Linux
+
+
+# -l
+# List the partition tables for the specified devices
+# and then exit.
+#
+
+
+# 查看硬盘和分区
+fdisk -l 
+
+fdisk -l /dev/sda
+
+
+
