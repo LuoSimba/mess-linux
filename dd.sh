fe0422219c21d2d340930e03f0b7480a4cb7535b
simba-luo <r.puppy@qq.com>
2021-05-11 18:52:45 +0800

    linux command dd

---
+++ dd.sh
@@ -0,0 +1,53 @@
+
+
+# dd - convert and copy a file
+#
+# Copy a file, converting and formatting according to
+# the operands.
+#
+
+
+# bs=BYTES
+# read and write BYTES bytes at a time (also see ibs=, obs=)
+
+# cbs=BYTES
+# convert BYTES bytes at a time
+
+# conv=CONVS
+# convert the file as per the comma separated symbol list
+
+# count=BLOCKS
+# copy only BLOCKS input blocks
+
+# ibs=BYTES
+# read BYTES bytes at a time (default: 512)
+
+# if=FILE
+# read from FILE instead of stdin
+
+# iflag=FLAGS
+# read as per the comma separrated symbol list
+
+# obs=BYTES
+# write BYTES bytes at a time (default: 512)
+
+
+# of=FILE
+# write to FILE instead of stdout
+
+# oflag=FLAGS
+# write as per the comma separated symbol list
+
+# seek=BLOCKS
+# skip BLOCKS obs-sized blocks at start of output
+
+# skip=BLOCKS
+# skip BLOCKS ibs-sized blocks at start of input
+
+# status=noxfer
+# suppress transfer statistics
+
+
+
+
+
