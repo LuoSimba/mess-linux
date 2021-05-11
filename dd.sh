89320e1bc7b20d2b0c06faa1b2699c913dac1d73
simba-luo <r.puppy@qq.com>
2021-05-11 20:57:37 +0800

    linux command dd

--- dd.sh
+++ dd.sh
@@ -7,8 +7,27 @@
 #
 
 
+# if=FILE
+# 从文件 FILE 中读取数据，而不是从 stdin
+
+# of=FILE
+# 写入到文件 FILE 中，而不是输出到 stdout
+
+
 # bs=BYTES
-# read and write BYTES bytes at a time (also see ibs=, obs=)
+# 每次读取和写入 BYTES 字节
+
+# ibs=BYTES
+# 每次读取 BYTES 字节（默认：512）
+
+# obs=BYTES
+# 每次写入 BYTES 字节（默认：512）
+
+# count=BLOCKS
+# 只复制 BLOCKS 次
+# 复制的总字节数 = ibs * count
+
+
 
 # cbs=BYTES
 # convert BYTES bytes at a time
@@ -16,24 +35,10 @@
 # conv=CONVS
 # convert the file as per the comma separated symbol list
 
-# count=BLOCKS
-# copy only BLOCKS input blocks
-
-# ibs=BYTES
-# read BYTES bytes at a time (default: 512)
-
-# if=FILE
-# read from FILE instead of stdin
-
 # iflag=FLAGS
 # read as per the comma separrated symbol list
 
-# obs=BYTES
-# write BYTES bytes at a time (default: 512)
 
-
-# of=FILE
-# write to FILE instead of stdout
 
 # oflag=FLAGS
 # write as per the comma separated symbol list
@@ -48,6 +53,24 @@
 # suppress transfer statistics
 
 
+
+
+# 从标准输入中读取，输出到标准输出
+echo "hello" | dd
+
+# 从文件中读取，输出到标准输出
+dd if=FILE
+
+# 从标准输入中读取，写入到文件中
+echo "hello" | dd of=FILE
+
+# 只从输入数据中取一次，每次取两个字节
+echo "aabbccdd" | dd bs=2 count=1
+# 结果为 "aa"
+
+
+# 复制磁盘的 MBR
+dd if=/dev/sda bs=512 count=1 > mbr.bin
 
 
 
