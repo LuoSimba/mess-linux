9ecbea7edca16ef014f50378dbc0bdeee3f51e9d
jim <r.puppy@qq.com>
2021-05-14 20:33:32 +0800

    linux command dd

--- dd.sh
+++ dd.sh
@@ -76,3 +76,28 @@ dd if=/dev/sda of=mbr.bin bs=512 count=1
 
 
 
+# Disk Speed Test: write to disk
+time dd if=/dev/zero of=a.bin bs=512 count=1000000
+
+ 1000000+0 records in
+ 1000000+0 records out
+ 512000000 bytes (512 MB, 488 MiB) copied, 1.77211 s, 289 MB/s
+
+ real	0m1.773s
+ user	0m0.455s
+ sys	0m1.316s
+
+
+time dd if=/dev/zero of=b.bin bs=4K count=125000
+
+ 125000+0 records in
+ 125000+0 records out
+ 512000000 bytes (512 MB, 488 MiB) copied, 0.324873 s, 1.6 GB/s
+
+ real	0m0.326s
+ user	0m0.056s
+ sys	0m0.270s
+
+
+
+
